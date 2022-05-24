b888b9c02218a69b4dedbe1bdfca61e97843d295
Jochen Topf <jochen@topf.org>
2022-05-24 21:10:20 +0200

    Use -s instead of --suffix for baseline command in test
    Because on Mac the command doesn't recognize --suffix

--- test/line-or-polygon.sh
+++ test/line-or-polygon.sh
@@ -12,7 +12,7 @@ SRCDIR="$1"
 mkdir -p line-or-polygon
 
 for input in "$SRCDIR"/test/line-or-polygon/*.opl; do
-    output="line-or-polygon/"$(basename --suffix=.opl "$input")
+    output="line-or-polygon/"$(basename -s .opl "$input")
     ../src/line-or-polygon -e "$SRCDIR/filter-patterns" "$input" >"$output.result"
     expected="$SRCDIR/test/$output.expected"
     echo diff -u "$expected" "$output.result"
